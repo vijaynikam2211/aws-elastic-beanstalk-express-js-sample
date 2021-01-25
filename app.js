349dd420b11389bf60609d7cac18e23c4b101821
Vijay Nikam <vijay.nikam@quickheal.com>
2021-01-25 19:43:10 +0530

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello Vijay Nikam!'));
+app.get('/', (req, res) => res.send('Hello Vijay Nikam - Retesting!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
